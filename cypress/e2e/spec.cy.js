2a72a839f7b81c5f805daa28c77308e866f29a8d
Nil Gallego Arcusa <nnggaa95@gmail.com>
2022-08-04 18:49:13 +0200

    + tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,53 @@
+var assert = require("assert");
+
 describe("empty spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io").mongoInsertOne("testcollection", {
-      testkey: "testvalue",
-    });
+    const coll = "testcollection";
+    cy.visit("https://example.cypress.io")
+      .mongoInsertOne(coll, {
+        testkey: "testvalue",
+      })
+      .then((res) => {
+        assert.ok(res.acknowledged);
+      })
+      .mongoFindOne(coll, { testkey: "testvalue" })
+      .then((res) => {
+        assert.ok(res._id);
+      })
+      .mongoUpdateOne(
+        coll,
+        { testkey: "testvalue" },
+        { $set: { testkey: "newtestvalue" } }
+      )
+      .then((res) => {
+        assert.equal(res.modifiedCount, 1);
+      })
+      .mongoDeleteOne(coll, { testkey: "newtestvalue" })
+      .then((res) => {
+        assert.equal(res.deletedCount, 1);
+      })
+      .mongoInsertMany(coll, [
+        {
+          testkey: "testvalue2",
+        },
+        {
+          testkey: "testvalue3",
+        },
+      ])
+      .then((res) => {
+        assert.ok(res.acknowledged);
+        assert.equal(res.insertedCount, 2);
+      })
+      .mongoFindMany(coll, { testkey: { $regex: "testvalue?" } })
+      .then((res) => {})
+      .mongoUpdateMany(
+        coll,
+        { testkey: { $regex: "testvalue?" } },
+        { $set: { testkey: "newtestvalue" } }
+      )
+      .mongoDeleteMany(coll, { testkey: { $regex: "newtestvalue" } })
+      .then((res) => {
+        assert.equal(res.deletedCount, 2);
+      });
   });
 });
